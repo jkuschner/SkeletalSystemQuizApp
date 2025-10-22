9e24ecc02c9eab54c325659c8b60b185cda18d5a
jkuschner <60696513+jkuschner@users.noreply.github.com>
2025-10-22 15:38:21 -0700

    Add Question data class and make list of sample questions

---
+++ app/src/main/java/com/example/quizapp/Question.kt
@@ -0,0 +1,8 @@
+package com.example.quizapp
+
+data class Question(
+    val id: Int,
+    val text: String,
+    val answers: List<String>,
+    val correctAnswer: String
+)
---
+++ app/src/main/java/com/example/quizapp/QuizData.kt
@@ -0,0 +1,28 @@
+package com.example.quizapp
+
+val sampleQuestions = listOf(
+    Question(
+        id = 1,
+        text = "What is the scientific name for the cheekbone?",
+        answers = listOf("Temporal bone", "Maxillae", "Sphenoid", "Zygomatic bone"),
+        correctAnswer = "Zygomatic bone"
+    ),
+    Question(
+        id = 2,
+        text = "What part of the body does the fundus belong to?",
+        answers = listOf("Spleen", "Heart", "Pancreas", "Stomach"),
+        correctAnswer = "Stomach"
+    ),
+    Question(
+        id = 3,
+        text = "How many bones are in an adult human body?",
+        answers = listOf("97", "360", "206", "186"),
+        correctAnswer = "206"
+    ),
+    Question(
+        id = 4,
+        text = "Which of these bones is NOT one of the 3 earbones?",
+        answers = listOf("Stapes", "Coccyx", "Incus", "Malleus"),
+        correctAnswer = "Coccyx"
+    )
+)
